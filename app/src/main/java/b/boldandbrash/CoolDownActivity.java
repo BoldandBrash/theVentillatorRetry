e36bfac24871323942f4a0127f36fd2064010839
TymanLS <sin.tyman@yahoo.com>
2017-10-08 04:24:44 -0700

    Coded Cooldown activity
    Changed strings.xml to include the coping questions, added content to
    .xml and .java files for the Cooldown activity, updated .gitignore to
    ignore .idea

--- app/src/main/java/b/boldandbrash/CoolDownActivity.java
+++ app/src/main/java/b/boldandbrash/CoolDownActivity.java
@@ -1,13 +1,23 @@
 package b.boldandbrash;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CoolDownActivity extends AppCompatActivity {
+
+    //create array of coping questions
+    private int i;
+    private TextView question_box = (TextView)findViewById(R.id.copingquestion);
+    private EditText answer_box = (EditText)findViewById(R.id.copingresponse);
+    private Resources res = getResources();
+    private String[] coping_questions = res.getStringArray(R.array.coping_questions);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +26,27 @@ public class CoolDownActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //intitalize variables to track coping questions
+        i = 0;
+        question_box.setText(coping_questions[i++]);
     }
+    public void fabClick(View v){
+        //don't know what this does
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
 
+        //changes question display and clears the EditText
+        question_box.setText(coping_questions[i++]);
+        answer_box.setText("");
+
+        //!!!add code to save responses to a file
+
+
+        if(i <= 6)
+        {
+            Intent intent = new Intent();
+
+            //!!!add more code to transfer to whichever activity comes next
+        }
+    }
 }
